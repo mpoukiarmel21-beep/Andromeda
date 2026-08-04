d53dcce86ca81dfdf6ce2faeaff57cbdccd689d0
mpoukiarmel21-beep <mpoukiarmel21@gmail.com>
2026-08-05 00:17:31 +0100

    Rebuild Per-Vector/Identifier Spoofing/Log Level cells in code: crash-proof action-sheet menus for Detection_Mode & Log_Level, programmatic text fields (no more broken PSLinkListCell/PSTextFieldCell from plist)

--- AndromedaSettings.bundle/RootListController.m
+++ AndromedaSettings.bundle/RootListController.m
@@ -1,4 +1,5 @@
 #import <Preferences/Preferences.h>
+#import <UIKit/UIKit.h>
 #import <spawn.h>
 #import <CoreFoundation/CoreFoundation.h>
 
@@ -7,13 +8,136 @@
 @interface AndromedaRootListController : PSListController
 @end
 
+static NSMutableDictionary* AndromedaLoadPrefs(void);
+static void AndromedaSavePrefs(NSMutableDictionary* prefs);
+
 @implementation AndromedaRootListController
 
 - (NSArray*)specifiers {
     if(!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+        NSArray* loaded = [self loadSpecifiersFromPlistName:@"Root" target:self];
+        NSMutableArray* merged = [NSMutableArray array];
+        for(PSSpecifier* spec in loaded) {
+            [merged addObject:spec];
+            NSString* groupName = [spec name];
+            if([groupName isEqualToString:@"Per-Vector Options"]) {
+                [merged addObjectsFromArray:[self perVectorSpecifiers]];
+            } else if([groupName isEqualToString:@"Identifier Spoofing"]) {
+                [merged addObjectsFromArray:[self identifierSpoofSpecifiers]];
+            } else if([groupName isEqualToString:@"Logs & Debug"]) {
+                [merged addObject:[self logLevelSpecifier]];
+            }
+        }
+        _specifiers = merged;
     }
     return _specifiers;
+}
+
+- (PSSpecifier*)specifierForKey:(NSString*)key {
+    for(PSSpecifier* spec in _specifiers) {
+        if([[spec propertyForKey:@"key"] isEqualToString:key]) return spec;
+    }
+    return nil;
+}
+
+- (PSSpecifier*)menuButtonForKey:(NSString*)key title:(NSString*)title defaultValue:(NSString*)defaultValue action:(SEL)action {
+    NSString* current = AndromedaLoadPrefs()[key];
+    if(!current) current = defaultValue;
+    NSString* label = current ? [NSString stringWithFormat:@"%@ (%@)", title, [current capitalizedString]] : title;
+    PSSpecifier* spec = [PSSpecifier preferenceSpecifierNamed:label target:self
+        set:nil get:nil detail:nil cell:PSButtonCell edit:nil];
+    [spec setProperty:key forKey:@"key"];
+    [spec setButtonAction:action];
+    return spec;
+}
+
+- (PSSpecifier*)textFieldForKey:(NSString*)key title:(NSString*)title placeholder:(NSString*)placeholder {
+    PSSpecifier* spec = [PSSpecifier preferenceSpecifierNamed:title target:self
+        set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:)
+        detail:nil cell:PSTextFieldCell edit:nil];
+    [spec setProperty:key forKey:@"key"];
+    if(placeholder) [spec setProperty:placeholder forKey:@"placeholder"];
+    return spec;
+}
+
+- (NSArray*)perVectorSpecifiers {
+    return @[
+        [self menuButtonForKey:@"Detection_Mode" title:@"Detection Response Mode" defaultValue:@"spoof" action:@selector(pickDetectionMode)],
+        [self textFieldForKey:@"FS_ExtraPaths" title:@"Extra Paths to Hide" placeholder:@"e.g. /var/jb/Applications/X.app,/private/var/mobile/xx"],
+        [self textFieldForKey:@"URL_ExtraSchemes" title:@"Extra URL Schemes" placeholder:@"e.g. cydia,sileo,zbra,filza,ssh"],
+        [self textFieldForKey:@"Dyld_ExtraLibs" title:@"Extra Libraries to Block" placeholder:@"e.g. libfrida,libsubstrate,dylib.dll"],
+        [self textFieldForKey:@"Proc_ExtraProcesses" title:@"Extra Processes to Hide" placeholder:@"e.g. frida-server,sshd,cycript"],
+        [self textFieldForKey:@"Class_ExtraPatterns" title:@"Extra Class Patterns" placeholder:@"e.g. MyDetector,JailbreakCheck"]
+    ];
+}
+
+- (NSArray*)identifierSpoofSpecifiers {
+    return @[
+        [self textFieldForKey:@"Spoof_Model" title:@"Device Model" placeholder:@"e.g. iPhone15,2 (leave empty to randomize)"],
+        [self textFieldForKey:@"Spoof_ProductType" title:@"Product Type" placeholder:@"e.g. iPhone15,2"],
+        [self textFieldForKey:@"Spoof_MachineName" title:@"Machine Name" placeholder:@"e.g. iPhone16,1"],
+        [self textFieldForKey:@"Spoof_SerialNumber" title:@"Serial Number" placeholder:@"12 alphanumeric chars"],
+        [self textFieldForKey:@"Spoof_UDID" title:@"UDID (40 hex)" placeholder:@"40 hex characters"],
+        [self textFieldForKey:@"Spoof_ECID" title:@"ECID (hex)" placeholder:@"e.g. 1234ABCDEF"],
+        [self textFieldForKey:@"Spoof_MLBSerial" title:@"MLB Serial Number" placeholder:@"e.g. F2LQ1234ABCD"],
+        [self textFieldForKey:@"Spoof_OSVersion" title:@"iOS Version" placeholder:@"e.g. 18.4.1"],
+        [self textFieldForKey:@"Spoof_BuildVersion" title:@"Build Version" placeholder:@"e.g. 22E240"],
+        [self textFieldForKey:@"Spoof_DeviceName" title:@"Device Name" placeholder:@"e.g. iPhone de Andromeda"],
+        [self textFieldForKey:@"Spoof_WiFiMAC" title:@"Wi-Fi MAC Address" placeholder:@"e.g. AA:BB:CC:DD:EE:FF"],
+        [self textFieldForKey:@"Spoof_BluetoothMAC" title:@"Bluetooth MAC Address" placeholder:@"e.g. AA:BB:CC:DD:EE:FF"]
+    ];
+}
+
+- (PSSpecifier*)logLevelSpecifier {
+    return [self menuButtonForKey:@"Log_Level" title:@"Log Level" defaultValue:@"info" action:@selector(pickLogLevel)];
+}
+
+- (void)pickDetectionMode {
+    [self showValuePickerForKey:@"Detection_Mode" title:@"Detection Response Mode"
+        options:@{ @"Hide": @"hide", @"Spoof": @"spoof", @"Block": @"block" }
+        defaultValue:@"spoof"];
+}
+
+- (void)pickLogLevel {
+    [self showValuePickerForKey:@"Log_Level" title:@"Log Level"
+        options:@{ @"None": @"none", @"Errors": @"errors", @"Info": @"info", @"Verbose": @"verbose" }
+        defaultValue:@"info"];
+}
+
+- (void)showValuePickerForKey:(NSString*)key title:(NSString*)title
+                      options:(NSDictionary<NSString*,NSString*>*)options
+                 defaultValue:(NSString*)defaultValue {
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:title
+        message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+
+    NSString* current = AndromedaLoadPrefs()[key];
+    if(!current) current = defaultValue;
+
+    for(NSString* display in options) {
+        NSString* value = options[display];
+        NSString* optTitle = (current && [current isEqualToString:value])
+            ? [display stringByAppendingString:@" ✓"] : display;
+        [alert addAction:[UIAlertAction actionWithTitle:optTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction* action) {
+            NSMutableDictionary* prefs = AndromedaLoadPrefs();
+            prefs[key] = value;
+            AndromedaSavePrefs(prefs);
+            PSSpecifier* spec = [self specifierForKey:key];
+            if(spec) {
+                [spec setName:[NSString stringWithFormat:@"%@ (%@)", title, display]];
+                [self reloadSpecifiers];
+            }
+        }]];
+    }
+
+    [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+
+    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        alert.popoverPresentationController.sourceView = self.view;
+        alert.popoverPresentationController.sourceRect = CGRectMake(self.view.bounds.size.width / 2.0, self.view.bounds.size.height / 2.0, 1.0, 1.0);
+        alert.popoverPresentationController.permittedArrowDirections = 0;
+    }
+
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 static NSMutableDictionary* AndromedaLoadPrefs(void) {
